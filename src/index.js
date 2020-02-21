8ad66da952e2965b1afd7e230dd02153b92f9106
Vladimir <simpleHelpD@yandex.ru>
2020-02-21 15:27:06 +1000

    Add props and prop-types

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDom from 'react-dom';
 import App from './components/app/app.jsx';
 
 const init = () => {
+  const settings = {
+    moviesTitle: [`Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`]
+  };
+
   ReactDom.render(
-      <App />,
+      <App
+        moviesTitle = {settings.moviesTitle}
+      />,
       document.querySelector(`#root`)
   );
 };
